02e0a8043e3668837a48182402aa32b95b915040
Jiri (George) Lebl <jiri.lebl@gmail.com>
2018-02-08 15:50:42 -0600

    fixup the last commit ... eeek,

--- getstats.sh
+++ getstats.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 echo
-<<<<<<< HEAD
 echo Chapters:"                     \t"`grep '^[\]chapter\**{' *.tex | wc -l`
 echo Sections:"                     \t"`grep '^[\]section\**{' *.tex | wc -l`
 echo
@@ -11,11 +10,6 @@ echo
 echo Figures:"                      \t"`grep '^[\]begin{\(figure\|diffyfloatingfigure\)' *.tex | wc -l`
 echo '\\---> which are simple insets:'"\t"`grep '^[\]begin{diffyfloatingfiguresimple' *.tex | wc -l`
 echo
-=======
-echo Exercises:"                    \t"`grep '^[\]begin{exercise' *.tex | wc -l`
-echo Figures:"                      \t"`grep '^[\]begin{\(figure\|diffyfloatingfigure\)' *.tex | wc -l`
-echo '\\---> which are simple insets:'"\t"`grep '^[\]begin{diffyfloatingfiguresimple' *.tex | wc -l`
->>>>>>> 330576e83ba7a57696e0ac59055726e59c52a1d6
 echo Tables:"                       \t"`grep '^[\]begin{table}' *.tex | wc -l`
 
 echo
