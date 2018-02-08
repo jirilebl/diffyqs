9ac8cc0727027aea7a0719629ae1f33d272f4d8c
Jiri (George) Lebl <jiri.lebl@gmail.com>
2018-02-08 15:41:52 -0600

    Gather stats, remove a comment

---
+++ getstats.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo
+echo Chapters:"                     \t"`grep '^[\]chapter\**{' *.tex | wc -l`
+echo Sections:"                     \t"`grep '^[\]section\**{' *.tex | wc -l`
+echo
+echo Exercises:"                    \t"`grep '^[\]begin{exercise' *.tex | wc -l`
+echo '\\---> with solutions:         '"\t"`grep '^[\]exsol' *.tex | wc -l`
+echo
+echo Figures:"                      \t"`grep '^[\]begin{\(figure\|diffyfloatingfigure\)' *.tex | wc -l`
+echo '\\---> which are simple insets:'"\t"`grep '^[\]begin{diffyfloatingfiguresimple' *.tex | wc -l`
+echo
+echo Tables:"                       \t"`grep '^[\]begin{table}' *.tex | wc -l`
+
+echo
