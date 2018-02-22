3685b59e8d8e991791ef8efbc68cac44ff4f2946
Jiri (George) Lebl <jiri.lebl@gmail.com>
2018-02-22 16:01:56 -0600

    fix tar generation, though probably jsut need to remove it

--- make-tar.sh
+++ make-tar.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-FILES=`git ls-files | fgrep -v '.gitignore' | fgrep -v 'make-tar.sh' | grep -v -- '^old/'`
+FILES=`git ls-files | fgrep -v '.gitignore' | fgrep -v 'make-tar.sh' | grep -v -- '^[a-z]*/'`
+FIGUREFILES=`git ls-files | fgrep -v '.gitignore' | fgrep -v 'make-tar.sh' | grep -- '^figures/'`
 
 rm -fR diffyqs
 mkdir diffyqs
+mkdir diffyqs/figures
 cp $FILES diffyqs
+cp $FIGUREFILES diffyqs/figures/
 
 rm -f diffyqs.tar.gz
 tar cvf diffyqs.tar diffyqs/
