83645f0efaad61cd91a69de1b313cc785fd88611
Jiri (George) Lebl <jiri.lebl@gmail.com>
2023-07-03 15:47:38 -0500

    update build script

--- slides/build.sh
+++ slides/build.sh
@@ -5,7 +5,8 @@ for n in *.tex ; do
 	echo =============================
 	p=`basename $n .tex`".pdf"
 	echo $n '-->' $p
-	rubber -d --synctex $n
+	#rubber -d --synctex $n
+	latexmk -pdf -synctex=1 "$n"
 done
 
 echo =============================
