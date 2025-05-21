8d3ec31c47da9883c3b66c090c42b32548b13dff
Jiri (George) Lebl <jiri.lebl@gmail.com>
2025-05-20 22:51:40 -0500

    Fixup getstats

--- getstats.sh
+++ getstats.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 echo
-echo Chapters:"                     \t"`grep '^[\]chapter\**{' *.tex | wc -l`
-echo Sections:"                     \t"`grep '^[\]section\**{' *.tex | wc -l`
+echo -e Chapters:"                     \t"`grep '^[\]chapter\**{' *.tex | wc -l`
+echo -e Sections:"                     \t"`grep '^[\]section\**{' *.tex | wc -l`
 echo
-echo Exercises:"                    \t"`grep '^[\]begin{exercise' *.tex | wc -l`
-echo '\\---> with solutions:         '"\t"`grep '^[\]exsol' *.tex | wc -l`
+echo -e Exercises:"                    \t"`grep '^[\]begin{exercise' *.tex | wc -l`
+echo -e '\\---> with solutions:         '"\t"`grep '^[\]exsol' *.tex | wc -l`
 echo
-echo Figures:"                      \t"`grep '^[\]begin{\(myfig\|mywrapfig\)' *.tex | wc -l`
-echo '\\---> which are simple insets:'"\t"`grep '^[\]begin{mywrapfigsimp' *.tex | wc -l`
+echo -e Figures:"                      \t"`grep '^[\]begin{\(myfig\|mywrapfig\)' *.tex | wc -l`
+echo -e '\\---> which are simple insets:'"\t"`grep '^[\]begin{mywrapfigsimp' *.tex | wc -l`
 echo
-echo Tables:"                       \t"`grep '^[\]begin{table}' *.tex | wc -l`
+echo -e Tables:"                       \t"`grep '^[\]begin{table}' *.tex | wc -l`
 echo
-echo Footnotes:"                    \t"`grep '[\]footnote{' *.tex | wc -l`
+echo -e Footnotes:"                    \t"`grep '[\]footnote{' *.tex | wc -l`
 
 echo
